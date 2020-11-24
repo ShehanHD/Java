5412aed3620151f2440120b1429fcfcd57ec44dd
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2020-11-24 22:42:26 +0100

    .

--- SocketInt/Client/Main.java
+++ SocketInt/Client/Main.java
@@ -1,13 +1,24 @@
 package SocketInt.Client;
 
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int x = 1;
+        Scanner scan = new Scanner(System.in);
+
+        while (x != 0) {
+            x = scan.nextInt();
+            send(x);
+        }
+    }
+
+    static void send(int num) {
         try {
             Socket clientServer = new Socket("localhost", 9999);
 
-            clientServer.getOutputStream().write(0);
+            clientServer.getOutputStream().write(num);
             clientServer.close();
         } catch (Exception e) {
             System.out.println(e);
